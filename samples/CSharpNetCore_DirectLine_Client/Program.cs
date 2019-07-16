e80926f539f0545e1acbce32589ba40e1150fe91
Jeff Derstadt <jeffders@microsoft.com>
2019-07-16 15:25:00 -0700

    Updates and samples

---
+++ samples/CSharpNetCore_DirectLine_Client/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector.DirectLine;
+
+namespace CSharpNetCore_DirectLine_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.WaitAll(Run());
+        }
+
+        public static async Task Run()
+        {
+            var endpoint = "https://<your_bot>.azurewebsites.net/.bot/";
+            var secret = "<direct_line_secret_value>";
+            var userId = Guid.NewGuid().ToString();
+            var userName = "Fred";
+
+            Console.WriteLine("Connecting...");
+
+            var tokenClient = new DirectLineClient(
+               new Uri(endpoint),
+               new DirectLineClientCredentials(secret));
+            var tokenConversation = await tokenClient.Tokens.GenerateTokenForNewConversationAsync().ConfigureAwait(false);
+
+            var client = new DirectLineClient(
+                new Uri(endpoint),
+                new DirectLineClientCredentials(tokenConversation.Token));
+
+            await client.StreamingConversations.ConnectAsync(
+                tokenConversation.ConversationId,
+                ReceiveActivities).ConfigureAwait(false);
+
+            var conversation = await client.StreamingConversations.StartConversationAsync().ConfigureAwait(false);
+            
+            Console.WriteLine($"Connected to conversation {conversation.ConversationId}");
+
+            Console.Write("> ");
+            var message = Console.ReadLine();
+
+            while (message != "end")
+            {
+                try
+                {
+                    var response = await client.StreamingConversations.PostActivityAsync(conversation.ConversationId,
+                        new Activity()
+                        {
+                            Type = "message",
+                            Text = message,
+                            From = new ChannelAccount()
+                            {
+                                Id = userId,
+                                Name = userName
+                            }
+                        }).ConfigureAwait(false);
+                }
+                catch (OperationException ex)
+                {
+                    Console.WriteLine($"OperationException when calling PostActivityAsync: ({ex.StatusCode})");
+                }
+                Console.Write("> ");
+                message = Console.ReadLine();
+            }
+
+            client.StreamingConversations.Disconnect();
+        }
+
+        public static void ReceiveActivities(ActivitySet activitySet)
+        {
+            if (activitySet != null)
+            {
+                foreach (var a in activitySet.Activities)
+                {
+                    if (a.Type == ActivityTypes.Message && a.From.Id.ToLowerInvariant().Contains("bot"))
+                    {
+                        Console.WriteLine($"<Bot>: {a.Text}");
+                        if (a.Attachments.Any())
+                        {
+                            foreach (var attachment in a.Attachments)
+                            {
+                                var stream = attachment.Content as Stream;
+                                Task.Run(async () => {
+                                    int count;
+                                    int total = 0;
+                                    byte[] buffer = new byte[4096];
+                                    do
+                                    {
+                                        count = await stream.ReadAsync(buffer, 0, 4096);
+                                        total += count;
+                                    } while (count > 0);
+                                    Console.WriteLine($"  Read stream of length: {total}");
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ samples/README.md
@@ -0,0 +1,7 @@
+# Samples
+
+This folder contains samples relating to the Direct Line App Service Extension Preview.
+
+Additional samples on how to update bot code can be found in the Bot Builder Samples repo here:
+
+https://github.com/microsoft/BotBuilder-Samples/tree/master/experimental/directline-app-service-extension
